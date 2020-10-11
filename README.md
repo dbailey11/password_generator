b418086cff1b9779efece2c976076af60fcd8115
Devyn Bailey <devynbailey11@yahoo.com>
2020-10-11 15:00:25 -0600

    added comments to js file, updated readme and added screenshot, relinked css and js files to html

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# password_generator
+# Javascript Password Generator
+
+## Purpose
+
+The purpose of this project was to build a password generator that would provide a completely random password based on criteria selected from the user. The program allows to you choose how many characters you would like from an given range, as well as choosing between letters, numbers, and special characters. The complete randomization of characters to be selected in the password provides greater security for companies or businesses that need to log in to high security sites.
+
+### Screenshots
+
+![Image of password generator](assets\images\password_generator_screenshot.png)
+
+## Installation
+
+To run the password generator please click on the leak below. No specific requirements to run application.
+
+- From GitHub [password_generator application](#).
+
+## Skills
+
+![badges](https://img.shields.io/badge/<WORD_ON_LEFT>-<WORD_ON_RIGHT>-informational?style=flat&logo=<LOGO_NAME>&logoColor=white&color=2bbc8a)
+
+## Resources (Documentation and other links)
+
+- "Javascript & JQuery, interactive front-end web development" - Jon Duckett
+- Class mates: Mychael Martinez and Michael Li
+- Tutor: Hannah Folk
+- Websites:
+  - w3schools.com
+  - https://stackoverflow.com/questions/1497481/javascript-password-generator
+  - Class activities: https://du.bootcampcontent.com/denver-coding-bootcamp/du-den-fsf-pt-09-2020-u-c/tree/master/Class-Content/03-JavaScript
+
+## License
+
+No license as this is a reference for others out there just getting started learning Javascript.
--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,5 @@
-// Assignment Code
-// var generateBtn = document.querySelector("#generate");
-// var upperCase = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// var lowerCase =  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-// var number = ["1,2,3,4,5,6,7,8,9,0"];
-// var specChr = ["!", "@", "#", "$", "&"];
-// var passwordChr = [];
 
+//Global variable decloration
 var generateBtn = document.querySelector("#generate");
 var upperCase = ["abcdefghizjklmnopqrstuvwxyz"];
 var lowerCase =  ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
@@ -22,21 +16,24 @@ passwordText.value = password;
 }
 
 
-//fucntion to generate password
+//Generate password function
 function generatePassword() {
+  //Promt asking user to input a value
   var passwordLength = prompt("How many characters would you want your password to be? Please choose between 8 and 128 characters.");
   console.log(passwordLength); 
-
+  //If statement checking valid user input and an alert if user input is invalid
   if (passwordLength < 8 || passwordLength > 128 ) {
     alert("Please enter a number between 8 and 128.");
     return'';
   } 
-
+  //Local variable decloration
   var confirmNum = confirm("Do you want numbers in your password?");
   var confirmLowerCase = confirm("Do you want lowercase characters in your password?");
   var confirmUpperCase = confirm("Do you want uppercase characters in your password?");
   var confirmSpecChar = confirm("Do you want special characters in your password?");
 
+  //If/Else statements with confirms asking user to select what character types they want in their password.
+  //Empty array concatination, building the new array that the for loop will use to select a randomized password.
   if (confirmUpperCase === true) { 
     var passOne = passwordChr.concat(upperCase);
     console.log("upperCase logic", upperCase);
@@ -65,21 +62,21 @@ function generatePassword() {
     var passFour = passThree.concat();
   }
 
-  
+  //If statement that prompts an alert if the user does not select any character types
   if (confirmUpperCase === false && confirmLowerCase === false && confirmNum === false && confirmSpecChar === false) {
     alert("Please select at least one character type");
     return('');
   }
 
-  //var passResult = passFour.concat(passResult);
+  //Varaible that makes the new concatenated array into a string
   var passFourString = passFour.toString();
-  //var passFourString = passFour();
+  //Variable to remove any commas so extra don't get randomly selected
   var passFourStringWithoutCommas = passFourString.replace(/,/g, '');
   console.log("no comma string", passFourStringWithoutCommas);
-
   console.log("final result", passFourString);
   var result = "";
 
+  //For loop for password randimzation
   for (var i = 0; i < passwordLength; i++) {
     var random = passFourString.charAt(Math.floor(Math.random() * passFourString.length));
     console.log(passFourString.length);
@@ -88,34 +85,15 @@ function generatePassword() {
     var result = random.concat(result);
 
   }
-
+  //calling the function
   return result;
 
 }
 
 //generatePassword ();
 
-
+//Click event
 generateBtn.addEventListener("click", writePassword);
 
 
 
-
-
-
-
-//randomization of characters selected 
-  // var randomNumber = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z, A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z, 1,2,3,4,5,6,7,8,9,0, !, @, #, $. &"]
- 
-  // var possibleCharacters = "abcdefghijklmnoprstuvwxyz0123456789"
-  // var randomNumber = Math.floor(Math.random() * possibleCharacters.length);
-  // var randomCharacter = possibleCharacters[randomNumber]
-  // console.log(possibleCharacters);
-  // return passwordText.value;
-
-
-
-//run function
-
-
-// Add event listener to generate button
